4210f4efab6f20fc3439080031c77247918dbdc8
Brian Lowe <brian.lowe@optimaenergy.net>
2018-02-22 10:30:39 +0000

    article controller

---
+++ web/Controllers/ArticleController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace web.Controllers
+{
+    public class ArticleController : Controller
+    {
+        [HttpGet]
+        [Route("blog/{filter?}")]
+        public IActionResult Index(string filter)
+        {
+            ViewBag.filter = filter;
+
+            return View();
+        }        
+        
+        [HttpGet]
+        [Route("{category}/{filter?}")]
+        public IActionResult Detail(string category, string filter)
+        {
+            ViewBag.category = category;
+            ViewBag.filter = filter;
+
+            return View();
+        }
+    }
+}
