596ff4b527a70b6bd1e725a01aefc806ed3a8e46
Brian Lowe <brian.lowe@optimaenergy.net>
2018-02-22 10:17:35 +0000

    static page stubs

--- web/Controllers/HomeController.cs
+++ web/Controllers/HomeController.cs
@@ -10,7 +10,43 @@ namespace web.Controllers
     public class HomeController : Controller
     {
         [HttpGet]
+        [Route("", Name = "Home_Index")]
         public IActionResult Index()
+        {
+            return View();
+        }        
+        
+        [HttpGet]
+        [Route("about", Name = "Home_About")]
+        public IActionResult About()
+        {
+            return View();
+        }        
+        
+        [HttpGet]
+        [Route("contact", Name = "Home_Contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("privacy", Name = "Home_Privacy")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("terms", Name = "Home_Terms")]
+        public IActionResult Terms()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("legal", Name = "Home_Legal")]
+        public IActionResult Legal()
         {
             return View();
         }
